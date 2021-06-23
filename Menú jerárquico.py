9591d659c4e409517c8984957acf0b2847ce517d
TheareDelgado <theare.delgado@alumnos.ucentral.cl>
2021-06-23 01:28:52 -0400

    Menú 2.0.2
    Cambios estéticos (íconos)

--- Menú jerárquico.py
+++ Menú jerárquico.py
@@ -22,11 +22,38 @@ arbol.insert("", END, text="Operaciones", iid=0, open=False)
 arbol.insert("", END, text="Clientes", iid=1, open=False)
 arbol.insert("", END, text="Funcionarios", iid=2, open=False)
 
-#sub-opciones operaciones                                              
-arbol.insert("", END, text="Depositar", iid=3, open=False)
-arbol.insert("", END, text="Retirar", iid=4, open=False)
-arbol.insert("", END, text="Transferir", iid=5, open=False)
-arbol.insert("", END, text="Préstamo", iid=6, open=False)
+#sub-opciones operaciones
+imagenCliente = Image.open("./man.png")
+imagenCliente =imagenCliente.resize((18,21),Image.ANTIALIAS)
+imagenCliente = ImageTk.PhotoImage(imagenCliente)
+
+imagenFuncionario = Image.open("./profile.png")
+imagenFuncionario =imagenFuncionario.resize((18,21),Image.ANTIALIAS)
+imagenFuncionario = ImageTk.PhotoImage(imagenFuncionario)
+
+
+imagenDepositar = Image.open("./depositar.png")
+imagenDepositar =imagenDepositar.resize((18,21),Image.ANTIALIAS)
+imagenDepositar = ImageTk.PhotoImage(imagenDepositar)
+
+imagenRetirar = Image.open("./retirar.png")
+imagenRetirar = imagenRetirar.resize((18,21),Image.ANTIALIAS)
+imagenRetirar = ImageTk.PhotoImage(imagenRetirar)
+
+
+imagenPrestamo = Image.open("./prestamo.png")
+imagenPrestamo = imagenPrestamo.resize((18,21),Image.ANTIALIAS)
+imagenPrestamo = ImageTk.PhotoImage(imagenPrestamo)
+
+imagenTransferir = Image.open("./transferir.png")
+imagenTransferir = imagenTransferir.resize((18,21),Image.ANTIALIAS)
+imagenTransferir = ImageTk.PhotoImage(imagenTransferir)
+
+
+arbol.insert("", END, text="Depositar", iid=3, open=False, image=imagenDepositar)
+arbol.insert("", END, text="Retirar", iid=4, open=False, image=imagenRetirar)
+arbol.insert("", END, text="Transferir", iid=5, open=False, image=imagenTransferir)
+arbol.insert("", END, text="Préstamo", iid=6, open=False, image=imagenPrestamo)
 
 arbol.move(3,0,0)  #iid 3 (hijo) se asocia al iid 0 (padre) en la posición 0
 arbol.move(4,0,1)
@@ -35,10 +62,10 @@ arbol.move(6,0,3)
 
 #sub opciones depositar
                                                                     #NOMBRE     RUT  MONTO
-arbol.insert("", END, text="Cliente n°1", iid=15, open=False, values=("Nombre","1-1","1"))
-arbol.insert("", END, text="Cliente n°2", iid=16, open=False, values=("Nombre","2-2","1"))
-arbol.insert("", END, text="Cliente n°3", iid=17, open=False, values=("Nombre","3-3","1"))
-arbol.insert("", END, text="Cliente n°4", iid=18, open=False, values=("Nombre","4-4","1"))
+arbol.insert("", END, text="Cliente n°1", iid=15, open=False, values=("Nombre","1-1","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°2", iid=16, open=False, values=("Nombre","2-2","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°3", iid=17, open=False, values=("Nombre","3-3","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°4", iid=18, open=False, values=("Nombre","4-4","1"), image=imagenCliente)
 
 arbol.move(15,3,0)
 arbol.move(16,3,1)
@@ -47,10 +74,10 @@ arbol.move(18,3,3)
 
 #sub opciones retirar
                                                                     #NOMBRE     RUT  MONTO
-arbol.insert("", END, text="Cliente n°1", iid=19, open=False, values=("Nombre","1-1","1"))
-arbol.insert("", END, text="Cliente n°2", iid=20, open=False, values=("Nombre","2-2","1"))
-arbol.insert("", END, text="Cliente n°3", iid=21, open=False, values=("Nombre","3-3","1"))
-arbol.insert("", END, text="Cliente n°4", iid=22, open=False, values=("Nombre","4-4","1"))
+arbol.insert("", END, text="Cliente n°1", iid=19, open=False, values=("Nombre","1-1","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°2", iid=20, open=False, values=("Nombre","2-2","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°3", iid=21, open=False, values=("Nombre","3-3","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°4", iid=22, open=False, values=("Nombre","4-4","1"), image=imagenCliente)
 
 arbol.move(19,4,0)
 arbol.move(20,4,1)
@@ -59,10 +86,10 @@ arbol.move(22,4,3)
 
 #sub opciones transferir
                                                                  #DE: NOMBRE     RUT  MONTO----->NOMBRE
-arbol.insert("", END, text="Cliente n°1", iid=23, open=False, values=("Nombre 1","1-1","1","Nombre 2"))
-arbol.insert("", END, text="Cliente n°2", iid=24, open=False, values=("Nombre 1","2-2","1","Nombre 2"))
-arbol.insert("", END, text="Cliente n°3", iid=25, open=False, values=("Nombre 1","3-3","1","Nombre 2"))
-arbol.insert("", END, text="Cliente n°4", iid=26, open=False, values=("Nombre 1","4-4","1","Nombre 2"))
+arbol.insert("", END, text="Cliente n°1", iid=23, open=False, values=("Nombre 1","1-1","1","Nombre 2"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°2", iid=24, open=False, values=("Nombre 1","2-2","1","Nombre 2"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°3", iid=25, open=False, values=("Nombre 1","3-3","1","Nombre 2"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°4", iid=26, open=False, values=("Nombre 1","4-4","1","Nombre 2"), image=imagenCliente)
 
 arbol.move(23,5,0)
 arbol.move(24,5,1)
@@ -71,10 +98,10 @@ arbol.move(26,5,3)
 
 #sub opciones prestamo
                                                                     #NOMBRE  MONTO CUOTAS
-arbol.insert("", END, text="Cliente n°1", iid=27, open=False, values=("Nombre","1","12"))
-arbol.insert("", END, text="Cliente n°2", iid=28, open=False, values=("Nombre","1","6"))
-arbol.insert("", END, text="Cliente n°3", iid=29, open=False, values=("Nombre","1","3"))
-arbol.insert("", END, text="Cliente n°4", iid=30, open=False, values=("Nombre","1","9"))
+arbol.insert("", END, text="Cliente n°1", iid=27, open=False, values=("Nombre","1","12"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°2", iid=28, open=False, values=("Nombre","1","6"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°3", iid=29, open=False, values=("Nombre","1","3"), image=imagenCliente)
+arbol.insert("", END, text="Cliente n°4", iid=30, open=False, values=("Nombre","1","9"), image=imagenCliente)
 
 arbol.move(27,6,0)
 arbol.move(28,6,1)
@@ -83,22 +110,22 @@ arbol.move(30,6,3)
 
 
 #sub-opciones clientes
-arbol.insert("", END, text="Cliente 1", iid=7, open=False, values=("Nombre ","Apellido ", "1-1","1"))
-arbol.insert("", END, text="Cliente 2", iid=8, open=False, values=("Nombre ","Apellido ", "2-2","1"))
-arbol.insert("", END, text="Cliente 3", iid=9, open=False, values=("Nombre ","Apellido ", "3-3","1"))
-arbol.insert("", END, text="Cliente 4", iid=10, open=False, values=("Nombre ","Apellido ", "4-4","1"))
-arbol.insert("", END, text="Cliente 5", iid=31, open=False, values=("Nombre ","Apellido ", "5-5","1"))
-arbol.insert("", END, text="Cliente 6", iid=32, open=False, values=("Nombre ","Apellido ", "6-6","1"))
-arbol.insert("", END, text="Cliente 7", iid=33, open=False, values=("Nombre ","Apellido ", "7-7","1"))
-arbol.insert("", END, text="Cliente 8", iid=34, open=False, values=("Nombre ","Apellido ", "8-8","1"))
-arbol.insert("", END, text="Cliente 9", iid=35, open=False, values=("Nombre ","Apellido ", "9-9","1"))
-arbol.insert("", END, text="Cliente 10", iid=36, open=False, values=("Nombre ","Apellido ", "10-1","1"))
-arbol.insert("", END, text="Cliente 11", iid=37, open=False, values=("Nombre ","Apellido ", "11-1","1"))
-arbol.insert("", END, text="Cliente 12", iid=38, open=False, values=("Nombre ","Apellido ", "12-2","1"))
-arbol.insert("", END, text="Cliente 13", iid=39, open=False, values=("Nombre ","Apellido ", "13-3","1"))
-arbol.insert("", END, text="Cliente 14", iid=40, open=False, values=("Nombre ","Apellido ", "14-4","1"))
-arbol.insert("", END, text="Cliente 15", iid=41, open=False, values=("Nombre ","Apellido ", "15-5","1"))
-arbol.insert("", END, text="Cliente 16", iid=42, open=False, values=("Nombre ","Apellido ", "16-6","1"))
+arbol.insert("", END, text="Cliente 1", iid=7, open=False, values=("Nombre ","Apellido ", "1-1","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 2", iid=8, open=False, values=("Nombre ","Apellido ", "2-2","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 3", iid=9, open=False, values=("Nombre ","Apellido ", "3-3","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 4", iid=10, open=False, values=("Nombre ","Apellido ", "4-4","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 5", iid=31, open=False, values=("Nombre ","Apellido ", "5-5","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 6", iid=32, open=False, values=("Nombre ","Apellido ", "6-6","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 7", iid=33, open=False, values=("Nombre ","Apellido ", "7-7","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 8", iid=34, open=False, values=("Nombre ","Apellido ", "8-8","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 9", iid=35, open=False, values=("Nombre ","Apellido ", "9-9","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 10", iid=36, open=False, values=("Nombre ","Apellido ", "10-1","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 11", iid=37, open=False, values=("Nombre ","Apellido ", "11-1","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 12", iid=38, open=False, values=("Nombre ","Apellido ", "12-2","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 13", iid=39, open=False, values=("Nombre ","Apellido ", "13-3","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 14", iid=40, open=False, values=("Nombre ","Apellido ", "14-4","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 15", iid=41, open=False, values=("Nombre ","Apellido ", "15-5","1"), image=imagenCliente)
+arbol.insert("", END, text="Cliente 16", iid=42, open=False, values=("Nombre ","Apellido ", "16-6","1"), image=imagenCliente)
 
 arbol.move(7,1,0)  
 arbol.move(8,1,1)
@@ -118,9 +145,9 @@ arbol.move(41,1,14)
 arbol.move(42,1,15)
 
 #sub-opciones funcionarios
-arbol.insert("", END, text="Funcionario 1", iid=11, open=False, values=("Nombre ","Apellido ", "1-1"))
-arbol.insert("", END, text="Funcionario 2", iid=12, open=False, values=("Nombre ","Apellido ", "2-2"))
-arbol.insert("", END, text="Funcionario 3", iid=13, open=False, values=("Nombre ","Apellido ", "3-3"))
+arbol.insert("", END, text="Funcionario 1", iid=11, open=False, values=("Nombre ","Apellido ", "1-1"), image=imagenFuncionario)
+arbol.insert("", END, text="Funcionario 2", iid=12, open=False, values=("Nombre ","Apellido ", "2-2"), image=imagenFuncionario)
+arbol.insert("", END, text="Funcionario 3", iid=13, open=False, values=("Nombre ","Apellido ", "3-3"), image=imagenFuncionario)
 
 arbol.move(11,2,0)  
 arbol.move(12,2,1)
