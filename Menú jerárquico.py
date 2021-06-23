a657216a2894cfc4c3e0daaf881e58a1c57535fc
TheareDelgado <theare.delgado@alumnos.ucentral.cl>
2021-06-23 00:59:43 -0400

    Menú 2.0.1
    Falta cambiar values por datos creíbles, todos los messagebox están listos

--- Menú jerárquico.py
+++ Menú jerárquico.py
@@ -17,12 +17,12 @@ root.resizable(width=False, height=False)
 arbol = ttk.Treeview(root)
 arbol.heading("#0", text="Banco", anchor="nw")
 
-#opciones
+#opciones                       
 arbol.insert("", END, text="Operaciones", iid=0, open=False)
 arbol.insert("", END, text="Clientes", iid=1, open=False)
 arbol.insert("", END, text="Funcionarios", iid=2, open=False)
 
-#sub-opciones operaciones
+#sub-opciones operaciones                                              
 arbol.insert("", END, text="Depositar", iid=3, open=False)
 arbol.insert("", END, text="Retirar", iid=4, open=False)
 arbol.insert("", END, text="Transferir", iid=5, open=False)
@@ -34,10 +34,11 @@ arbol.move(5,0,2)
 arbol.move(6,0,3)
 
 #sub opciones depositar
-arbol.insert("", END, text="Cliente n°1", iid=15, open=False)
-arbol.insert("", END, text="Cliente n°2", iid=16, open=False)
-arbol.insert("", END, text="Cliente n°3", iid=17, open=False)
-arbol.insert("", END, text="Cliente n°4", iid=18, open=False)
+                                                                    #NOMBRE     RUT  MONTO
+arbol.insert("", END, text="Cliente n°1", iid=15, open=False, values=("Nombre","1-1","1"))
+arbol.insert("", END, text="Cliente n°2", iid=16, open=False, values=("Nombre","2-2","1"))
+arbol.insert("", END, text="Cliente n°3", iid=17, open=False, values=("Nombre","3-3","1"))
+arbol.insert("", END, text="Cliente n°4", iid=18, open=False, values=("Nombre","4-4","1"))
 
 arbol.move(15,3,0)
 arbol.move(16,3,1)
@@ -45,10 +46,11 @@ arbol.move(17,3,2)
 arbol.move(18,3,3)
 
 #sub opciones retirar
-arbol.insert("", END, text="Cliente n°1", iid=19, open=False)
-arbol.insert("", END, text="Cliente n°2", iid=20, open=False)
-arbol.insert("", END, text="Cliente n°3", iid=21, open=False)
-arbol.insert("", END, text="Cliente n°4", iid=22, open=False)
+                                                                    #NOMBRE     RUT  MONTO
+arbol.insert("", END, text="Cliente n°1", iid=19, open=False, values=("Nombre","1-1","1"))
+arbol.insert("", END, text="Cliente n°2", iid=20, open=False, values=("Nombre","2-2","1"))
+arbol.insert("", END, text="Cliente n°3", iid=21, open=False, values=("Nombre","3-3","1"))
+arbol.insert("", END, text="Cliente n°4", iid=22, open=False, values=("Nombre","4-4","1"))
 
 arbol.move(19,4,0)
 arbol.move(20,4,1)
@@ -56,10 +58,11 @@ arbol.move(21,4,2)
 arbol.move(22,4,3)
 
 #sub opciones transferir
-arbol.insert("", END, text="Cliente n°1", iid=23, open=False)
-arbol.insert("", END, text="Cliente n°2", iid=24, open=False)
-arbol.insert("", END, text="Cliente n°3", iid=25, open=False)
-arbol.insert("", END, text="Cliente n°4", iid=26, open=False)
+                                                                 #DE: NOMBRE     RUT  MONTO----->NOMBRE
+arbol.insert("", END, text="Cliente n°1", iid=23, open=False, values=("Nombre 1","1-1","1","Nombre 2"))
+arbol.insert("", END, text="Cliente n°2", iid=24, open=False, values=("Nombre 1","2-2","1","Nombre 2"))
+arbol.insert("", END, text="Cliente n°3", iid=25, open=False, values=("Nombre 1","3-3","1","Nombre 2"))
+arbol.insert("", END, text="Cliente n°4", iid=26, open=False, values=("Nombre 1","4-4","1","Nombre 2"))
 
 arbol.move(23,5,0)
 arbol.move(24,5,1)
@@ -67,10 +70,11 @@ arbol.move(25,5,2)
 arbol.move(26,5,3)
 
 #sub opciones prestamo
-arbol.insert("", END, text="Cliente n°1", iid=27, open=False)
-arbol.insert("", END, text="Cliente n°2", iid=28, open=False)
-arbol.insert("", END, text="Cliente n°3", iid=29, open=False)
-arbol.insert("", END, text="Cliente n°4", iid=30, open=False)
+                                                                    #NOMBRE  MONTO CUOTAS
+arbol.insert("", END, text="Cliente n°1", iid=27, open=False, values=("Nombre","1","12"))
+arbol.insert("", END, text="Cliente n°2", iid=28, open=False, values=("Nombre","1","6"))
+arbol.insert("", END, text="Cliente n°3", iid=29, open=False, values=("Nombre","1","3"))
+arbol.insert("", END, text="Cliente n°4", iid=30, open=False, values=("Nombre","1","9"))
 
 arbol.move(27,6,0)
 arbol.move(28,6,1)
@@ -79,22 +83,22 @@ arbol.move(30,6,3)
 
 
 #sub-opciones clientes
-arbol.insert("", END, text="Cliente 1", iid=7, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 2", iid=8, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 3", iid=9, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 4", iid=10, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 5", iid=31, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 6", iid=32, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 7", iid=33, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 8", iid=34, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 9", iid=35, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 10", iid=36, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 11", iid=37, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 12", iid=38, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 13", iid=39, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 14", iid=40, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 15", iid=41, open=False, values=("Nombre ","Apellido ", "1","1"))
-arbol.insert("", END, text="Cliente 16", iid=42, open=False, values=("Nombre ","Apellido ", "1","1"))
+arbol.insert("", END, text="Cliente 1", iid=7, open=False, values=("Nombre ","Apellido ", "1-1","1"))
+arbol.insert("", END, text="Cliente 2", iid=8, open=False, values=("Nombre ","Apellido ", "2-2","1"))
+arbol.insert("", END, text="Cliente 3", iid=9, open=False, values=("Nombre ","Apellido ", "3-3","1"))
+arbol.insert("", END, text="Cliente 4", iid=10, open=False, values=("Nombre ","Apellido ", "4-4","1"))
+arbol.insert("", END, text="Cliente 5", iid=31, open=False, values=("Nombre ","Apellido ", "5-5","1"))
+arbol.insert("", END, text="Cliente 6", iid=32, open=False, values=("Nombre ","Apellido ", "6-6","1"))
+arbol.insert("", END, text="Cliente 7", iid=33, open=False, values=("Nombre ","Apellido ", "7-7","1"))
+arbol.insert("", END, text="Cliente 8", iid=34, open=False, values=("Nombre ","Apellido ", "8-8","1"))
+arbol.insert("", END, text="Cliente 9", iid=35, open=False, values=("Nombre ","Apellido ", "9-9","1"))
+arbol.insert("", END, text="Cliente 10", iid=36, open=False, values=("Nombre ","Apellido ", "10-1","1"))
+arbol.insert("", END, text="Cliente 11", iid=37, open=False, values=("Nombre ","Apellido ", "11-1","1"))
+arbol.insert("", END, text="Cliente 12", iid=38, open=False, values=("Nombre ","Apellido ", "12-2","1"))
+arbol.insert("", END, text="Cliente 13", iid=39, open=False, values=("Nombre ","Apellido ", "13-3","1"))
+arbol.insert("", END, text="Cliente 14", iid=40, open=False, values=("Nombre ","Apellido ", "14-4","1"))
+arbol.insert("", END, text="Cliente 15", iid=41, open=False, values=("Nombre ","Apellido ", "15-5","1"))
+arbol.insert("", END, text="Cliente 16", iid=42, open=False, values=("Nombre ","Apellido ", "16-6","1"))
 
 arbol.move(7,1,0)  
 arbol.move(8,1,1)
@@ -114,9 +118,9 @@ arbol.move(41,1,14)
 arbol.move(42,1,15)
 
 #sub-opciones funcionarios
-arbol.insert("", END, text="Funcionario 1", iid=11, open=False, values=("Nombre ","Apellido ", "1"))
-arbol.insert("", END, text="Funcionario 2", iid=12, open=False, values=("Nombre ","Apellido ", "1"))
-arbol.insert("", END, text="Funcionario 3", iid=13, open=False, values=("Nombre ","Apellido ", "1"))
+arbol.insert("", END, text="Funcionario 1", iid=11, open=False, values=("Nombre ","Apellido ", "1-1"))
+arbol.insert("", END, text="Funcionario 2", iid=12, open=False, values=("Nombre ","Apellido ", "2-2"))
+arbol.insert("", END, text="Funcionario 3", iid=13, open=False, values=("Nombre ","Apellido ", "3-3"))
 
 arbol.move(11,2,0)  
 arbol.move(12,2,1)
@@ -138,43 +142,43 @@ def itemSeleccionado(event):
         
         #IF para los Clientes de DEPOSITAR
         if iid=="15":
-             messagebox.showinfo("Datos Deposito n°1", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°1", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
         if iid=="16":
-             messagebox.showinfo("Datos Deposito n°2", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°2", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
         if iid=="17":
-             messagebox.showinfo("Datos Deposito n°3", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°3", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
         if iid=="18":
-             messagebox.showinfo("Datos Deposito n°4", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°4", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
 
         #IF para los Clientes de RETIRAR
         if iid=="19":
-             messagebox.showinfo("Datos Retiro n°1", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°1", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
         if iid=="20":
-             messagebox.showinfo("Datos Retiro n°2", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°2", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
         if iid=="21":
-             messagebox.showinfo("Datos Retiro n°3", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°3", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
         if iid=="22":
-             messagebox.showinfo("Datos Retiro n°4", "Rellena esto")
+             messagebox.showinfo("Datos Deposito n°4", "Nombre:"+valor[0]+"     Rut:"+str(valor[1])+"     Monto:"+str(valor[2]))
 
         #IF para los Clientes de TRANSFERENCIA
         if iid=="23":
-             messagebox.showinfo("Datos Transferencia n°1", "Rellena esto")
+             messagebox.showinfo("Datos Transferencia n°1", "Nombre:"+valor[0]+"    Rut:"+str(valor[1])+"     Monto:"+str(valor[2])+"-------->"+valor[3])
         if iid=="24":
-             messagebox.showinfo("Datos Transferencia n°2", "Rellena esto")
+             messagebox.showinfo("Datos Transferencia n°2", "Nombre:"+valor[0]+"    Rut:"+str(valor[1])+"     Monto:"+str(valor[2])+"-------->"+valor[3])
         if iid=="25":
-             messagebox.showinfo("Datos Transferencia n°3", "Rellena esto")
+             messagebox.showinfo("Datos Transferencia n°3", "Nombre:"+valor[0]+"    Rut:"+str(valor[1])+"     Monto:"+str(valor[2])+"-------->"+valor[3])
         if iid=="26":
-             messagebox.showinfo("Datos Transferencia n°4", "Rellena esto")
+             messagebox.showinfo("Datos Transferencia n°4", "Nombre:"+valor[0]+"    Rut:"+str(valor[1])+"     Monto:"+str(valor[2])+"-------->"+valor[3])
 
         #IF para los Clientes de PRÉSTAMO
         if iid=="27":
-             messagebox.showinfo("Datos Préstamo n°1", "Rellena esto")
+             messagebox.showinfo("Datos Préstamo n°1", "Nombre:"+valor[0]+"     Monto solicitado:"+str(valor[1])+"     N° Cuotas:"+str(valor[2]))
         if iid=="28":
-             messagebox.showinfo("Datos Préstamo n°2", "Rellena esto")
+             messagebox.showinfo("Datos Préstamo n°2", "Nombre:"+valor[0]+"     Monto solicitado:"+str(valor[1])+"     N° Cuotas:"+str(valor[2]))
         if iid=="29":
-             messagebox.showinfo("Datos Préstamo n°3", "Rellena esto")
+             messagebox.showinfo("Datos Préstamo n°3", "Nombre:"+valor[0]+"     Monto solicitado:"+str(valor[1])+"     N° Cuotas:"+str(valor[2]))
         if iid=="30":
-             messagebox.showinfo("Datos Préstamo n°4", "Rellena esto")
+             messagebox.showinfo("Datos Préstamo n°4", "Nombre:"+valor[0]+"     Monto solicitado:"+str(valor[1])+"     N° Cuotas:"+str(valor[2]))
 
 
 
